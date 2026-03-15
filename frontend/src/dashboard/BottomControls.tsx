3329516d982ebf4705055e2d9e63899dc5659b7e
pakhunchan <pakhun.chan@gmail.com>
2026-03-15 17:54:40 -0500

    Add "End Session" text label to bottom controls button

--- frontend/src/dashboard/BottomControls.tsx
+++ frontend/src/dashboard/BottomControls.tsx
@@ -46,6 +46,7 @@ export default function BottomControls({
           <path d="M16.5 9.4l-9-5.19M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" />
           <line x1="3" y1="3" x2="21" y2="21" strokeWidth="2.5" />
         </svg>
+        End Session
       </button>
     </div>
   );
